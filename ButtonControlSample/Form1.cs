d91fb47a8aedad9021b084f81b9912dfb6bacad3
ThansiJamal <thansijamal18@gmail.com>
2020-10-12 10:03:49 -0700

    first commit

---
+++ ButtonControlSample/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonControlSample
+{
+    public partial class Form1 : Form
+    {
+        int i=1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (i <= 10)
+            {
+                Button b = new Button();
+                Controls.Add(b);
+                b.Location = new Point(e.X, e.Y);
+                b.Text = i.ToString();
+                b.BackColor = Color.Aqua;
+            }
+            i++;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                
+                
+            }
+            
+        }
+    }
+}
